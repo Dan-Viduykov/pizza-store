7461b3c8928a051131df3dc66e269b1f763f36df
Danya_Vid <danilviduykov@gmail.com>
2022-09-04 21:21:37 +0500

    some add

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,16 +1,22 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretSquareUp } from "@fortawesome/free-regular-svg-icons";
+import { faCaretSquareUp, faSquareCaretDown } from "@fortawesome/free-regular-svg-icons";
 import SortList from "./SortList";
 import styles from "./Sort.module.scss";
 
 const Sort: FC = () => {
+    const [ active, setActive ] = useState(false);
+
+    const handleClick = () => {
+        setActive(state => !state)
+    }
+
     return (
         <div className={styles.sort}>
-            <FontAwesomeIcon className={styles.sort__icon} icon={faCaretSquareUp} />
+            <FontAwesomeIcon className={styles.sort__icon} icon={active ? faSquareCaretDown : faCaretSquareUp} />
             <span className={styles.sort__text}>Сортировка по:</span>
-            <button className={styles.sort__button}>популярности</button>
-            <SortList active={true} />
+            <button className={styles.sort__button} onClick={handleClick}>популярности</button>
+            <SortList active={active} />
         </div>
     )
 }
