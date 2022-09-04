d0d78ebe1a30b4dfc54cbca23a32c39f9dcfb4af
Danya_Vid <danilviduykov@gmail.com>
2022-09-04 13:41:04 +0500

    add categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,13 +2,24 @@ import { FC } from "react";
 import styles from "./Categories.module.scss";
 
 const Categories: FC = () => {
+    const uuid = require('uuid')
     const catogories = ['Всё', 'Мясные', 'Вегитарианские', 'Гриль', 'Острые', 'Закрытые'];
-    const items = catogories.map(item => <li key={item} className={styles.categories__item}>{item}</li>);
+
+    const createItem = (title: string) => {
+        const id = uuid.v4();
+
+        return (
+            <li key={title} className={styles.categories__item}>
+                <input className={styles.categories__input} id={id} type="radio" name="category" />
+                <label className={styles.categories__label} htmlFor={id}>{title}</label>
+            </li>
+        )
+    }
 
     return (
         <div className={styles.categories}>
             <ul className={styles.categories__list}>
-                {items}
+                {catogories.map(createItem)}
             </ul>
         </div>
     )
--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,10 +1,18 @@
+import Categories from "@/components/Categories";
+import PizzaList from "@/components/PizzaList";
+import Sort from "@/components/Sort";
 import { FC } from "react";
 import styles from "./Home.module.scss";
 
 const Home: FC = () => {
     return (
         <div className={styles.home}>
-            Home
+            <div className={styles.home__actions}>
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className={styles.home__title}>Все пиццы</h2>
+            <PizzaList />
         </div>
     )
 }
