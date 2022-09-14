b159ca452cad9fa687c8d8fd006595d9568a9068
Danya_Vid <danilviduykov@gmail.com>
2022-09-14 15:38:44 +0500

    add fetching

--- src/components/PizzaList/PizzaList.tsx
+++ src/components/PizzaList/PizzaList.tsx
@@ -7,12 +7,12 @@ import { useTypedSelector } from "@/hooks/useTypedSelector";
 
 const PizzaList: FC = () => {
     const { filter, sorting } = useTypedSelector(state => state.filterReducer)
-    const { isLoading, isError, data: pizzas } = useGetAllPizzasQuery({sorting, filter})
+    const { isLoading, isError, isFetching, data: pizzas } = useGetAllPizzasQuery({sorting, filter})
 
     const skeletons = [...new Array(4)].map((item, idx) => <Skeleton key={idx} />)
     const pizzasElements = pizzas?.map(item => <PizzaCard key={item.id} className={styles.list__item} pizza={item} />)
 
-    const content = isLoading ? skeletons : pizzasElements 
+    const content = isLoading || isFetching ? skeletons : pizzasElements 
 
     return (
         <ul className={styles.list}>
