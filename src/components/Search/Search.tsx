16710a7a3ee28c832fbec060ea4b2c780ea0028f
Danya_Vid <danilviduykov@gmail.com>
2022-09-15 10:44:42 +0500

    add controll value

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,25 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Search.module.scss";
+import { useActions } from "@/hooks/useActions";
 
 interface SearchProps {
     className?: string;
 }
 
 const Search: FC<SearchProps> = ({className}) => {
+    const [ value, setValue ] = useState('');
+
     return (
         <div className={`${styles.wrap} ${className}`}>   
             <FontAwesomeIcon className={styles.icon} icon={faSearch} />
             <input
                 className={styles.input}
                 type="text"
-                placeholder="Поиск пиццы ..." />
+                placeholder="Поиск пиццы ..."
+                onChange={(e) => setValue(e.target.value)}
+                value={value} />
         </div>
     )
 }
