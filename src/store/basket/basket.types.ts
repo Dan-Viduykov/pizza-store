a034a4a163d183a05361ac6da05915ef1497cc40
Danya_Vid <danilviduykov@gmail.com>
2022-09-26 13:23:23 +0500

    add added count and price for basket

--- src/store/basket/basket.types.ts
+++ src/store/basket/basket.types.ts
@@ -1,6 +1,16 @@
-import { IPizza } from "@/services/pizza.types";
+export interface IBaksetPizza {
+    id: number;
+    title: string;
+    size: number;
+    thickness: string;
+    price: number;
+    imageUrl: string;
+    category: string;
+    rating: number;
+    count: number;
+}   
 
 export interface basketState {
-    items: IPizza[],
+    items: IBaksetPizza[],
     totalPrice: number;
 }  
